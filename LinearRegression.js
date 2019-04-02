de5296aa272db1b5f3e45b3d6c4e92c30e0ccf2c
asdf1899 <araid.anas99@gmail.com>
2019-04-03 00:09:01 +0200

    Add medie and sommaProdotto

--- LinearRegression.js
+++ LinearRegression.js
@@ -1,8 +1,39 @@
 class LinearRegression{
-    constructor (x, y){
-        if (!isArray(x) || !isArray(y)){
-            return false;
-        }
-        
+  constructor (x, y){
+    if (! Array.isArray(x) || !Array.isArray(y)){
+        return false;
     }
+    this.x_dataset = x;
+    this.y_dataset = y;
+  }
+  getSommaProdotto(){
+    var somma=0;
+    for (var i=0; i < this.x_dataset.length;i++){
+      somma += this.x_dataset[i] * this.y_dataset[i];
+    }
+    return somma;
+  }
+  getXMedia(){
+    var m=0;
+    for (var i=0; i<this.x_dataset.length;i++){
+      m += this.x_dataset[i];
+    }
+    return m / this.x_dataset.length;
+  }
+  getYMedia(){
+    var m=0;
+    for (var i=0; i<this.y_dataset.length;i++){
+      m += this.y_dataset[i];
+    }
+    return m / this.y_dataset.length;
+  }
+  getCovarianza(){
+    return this.x_dataset;
+  }
+  getVarianza(){
+    return;
+  }
+  predict(x_predict){
+
+  }
 }
